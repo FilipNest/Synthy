f90b9bfcdb043862ff1377a1ddf70fbdc6b00be6
Filip Hnízdo <filip@bluejumpers.com>
2015-01-27 22:30:54 +0000

    Synthy has a long term memory

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
+//Listen to incoming connections on port 1337
+
 app.listen(1337);
+
+//Make a new nedb database to store pushed patterns in
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: 'memory', autoload: true });
+
+//RAM storage of the last pattern that was pushed. Eventually can replace this with a playlist of all pushed entries.
 
 var previous = null;
 
-function handler (req, res) {
-    res.writeHead(200);
-    console.log(previous);
-    res.end(previous);
-  };
-
 io.on('connection', function (socket) {
-socket.emit('synth', previous);
-  socket.on('bundle', function (data) {
-    previous = data;
-    io.sockets.emit('synth', previous);
-  });
+
+    socket.emit('synth', previous);
+
+    //On receiving a pattern, save it in memory and the database and emit it to everyone on the page
+
+    socket.on('bundle', function (data) {
+
+        previous = data;
+        db.insert(data);
+
+        io.sockets.emit('synth', previous);
+      });
 });

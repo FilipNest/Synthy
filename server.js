d1030cbfddbd2394b4fc584d036bfe0f2807504d
Filip Hnízdo <filip@bluejumpers.com>
2015-01-28 20:14:39 +0000

    Numbering in database

--- server.js
+++ server.js
@@ -12,21 +12,15 @@ function handler (req, res) {};
 var Datastore = require('nedb')
   , db = new Datastore({ filename: 'memory', autoload: true });
 
-//RAM storage of the last pattern that was pushed. Eventually can replace this with a playlist of all pushed entries.
+//Variable for the last pattern that was pushed.
 
-var latest = null;
+var latest;
 
-db.find({}).sort({_id:1}).skip(0).limit(1).exec(function (err, docs) {
+db.find({}).sort({number:1}).skip(0).limit(1).exec(function (err, docs) {
   
     latest = docs[0];
-    
-    //Get count of patterns
-
-    db.count({}, function (err, count) {
-      latest.count = count;
-    });
         
-    });   
+});   
 
 io.on('connection', function (socket) {
 
@@ -36,18 +30,23 @@ io.on('connection', function (socket) {
 
     socket.on('bundle', function (data) {
 
-        var oldcount = latest.count;
-        latest = data;
-        latest.count = oldcount+1;
-        db.insert(data);
+        //Get count of patterns
+
+    db.count({}, function (err, count) {
+      data.number = count+1;
+      latest = data;
+    db.insert(data);
 
         io.sockets.emit('synth', latest);
+        
+    });
+        
       });
     
     //Call to get another bundle from database
    socket.on('memory', function(data){
       
-    db.find({}).sort({_id:-1}).skip(data).limit(1).exec(function (err, docs) {
+    db.find({}).sort({number:-1}).skip(data).limit(1).exec(function (err, docs) {
   
     socket.emit('synth',docs[0]);    
         
